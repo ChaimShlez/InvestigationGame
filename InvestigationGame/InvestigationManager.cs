b3e6f4007fb8c3736cb7c8ee2d74dd12f35e7751
Chaim Shlez <chaimshlez@gmail.com>
2025-06-16 15:33:28 +0300

    create_maneger

--- InvestigationGame/InvestigationManager.cs
+++ InvestigationGame/InvestigationManager.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InvestigationGame.Base;
+using InvestigationGame.Enum;
 
 namespace InvestigationGame
 {
-    internal class Maneger
+    internal class InvestigationManager
     {
+
+        private IranianAgent Agent;
+
+        public InvestigationManager(IranianAgent agent)
+        {
+            Agent = agent;
+        }
+
+        public void AttachSensor(EnumTypeSensor sensorType)
+        {
+            Sensor sensor = SensorFactory.CreateSensor(sensorType);
+            Agent.AttachSensor(sensor);
+
+        }
+
+
+        public void Game()
+        {
+
+            int maxChoice = 10;
+
+            while(maxChoice > 0)
+            {
+                $"1. choice {EnumTypeSensor.AudioSensor}\n" +
+                $"2. choice {EnumTypeSensor.ThermalSensor}\n" +
+                $"3. choice {EnumTypeSensor.PulseSensor}");
+
+            }
+        }
     }
 }
